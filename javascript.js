a357d91479cd10f5d669dfc27ad3e684b441c4c2
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-17 16:36:29 +0100

    17,01,16,36

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("fsfhgjgd")
+console.log("fgdd")
+
+
+$("#d1").mouseover(function(){
+    console.log("on on on on ");
+    $(".subMenu").css("display", "block")
+})
+
+$(".topnavmenu").mouseleave(function(){
+    console.log("off off off ");
+    $(".subMenu").css("display", "none")
+})
