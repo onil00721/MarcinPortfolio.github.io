4989989861fe664395692920370e710d62f74d72
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-18 15:31:49 +0100

    18,01,15,31

--- javascript.js
+++ javascript.js
@@ -1,12 +1,30 @@
 console.log("fgdd")
 
 
-$("#d1").mouseover(function(){
+$("#m1").mouseover(function(){
     console.log("on on on on ");
-    $(".subMenu").css("display", "block")
+    $(".subMenu1").css("display", "block")
 })
+
+$("#m2").mouseover(function(){
+    console.log("on on on on ");
+    $(".subMenu2").css("display", "block")
+})
+
+$("#m3").mouseover(function(){
+    console.log("on on on on ");
+    $(".subMenu3").css("display", "block")
+})
+
+$("#m4").mouseover(function(){
+    console.log("on on on on ");
+    $(".subMenu4").css("display", "block")
+})
+
+
+
 
 $(".topnavmenu").mouseleave(function(){
     console.log("off off off ");
-    $(".subMenu").css("display", "none")
+    $(".subMenu1, .subMenu2, .subMenu3, .subMenu4").css("display", "none")
 })
